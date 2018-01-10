e2dc3061029db91e458123ee8b6ce621aadb52c0
Hariprashanth <hari@lavalabs.net>
2018-01-10 15:52:27 +0800

    hari changes

---
+++ src/main/webapp/app/public/package/package.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-package',
+  templateUrl: './package.component.html',
+  styles: []
+})
+export class PackageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
